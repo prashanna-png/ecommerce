a5773be43a7e8bc27e3a83c525768752e0f946da
prashanna <prashandon97@gmail.com>
2026-08-25 11:51:50 +0545

    add products schema

---
+++ backend/model/Products.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+
+const productSchema = new mongoose.Schema({
+  name:{
+    type:String,
+    required: true
+  },
+  description:{
+    type:String
+  },
+  price:{
+    type: Number,
+    required: true
+  },
+  brand:{
+    type:String,
+    required:true
+  },
+  category:{
+    type:String,
+    required:true
+  },
+  rating:{
+    type:Number,
+    default:0
+  },
+  numReviews:{
+    type: String,
+    default:0
+  },
+  image:{
+    type:String,
+    default: "/images/sample.jpg",
+  },
+  countInStock:{
+    type:Number,
+    default:0
+  },
+})
