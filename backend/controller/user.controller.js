18ad9a08265415389790dd60380ee5a1e5ae34e2
prashanna <prashandon97@gmail.com>
2026-08-26 18:22:38 +0545

    use hash function for password

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,11 +1,20 @@
 import User from '../model/user.js';
+import bcrypt from 'bcrypt';
 
 const signup = async (req,res)=>{
   const {fullName, email, password,isAdmin} = req.body;
   const user = await User.findOne({email});
-  if(user) return res.status(400).send({error:'user already exist'});
 
-  const newUser = await User.create({fullName,email,password,isAdmin});
+  if(user) 
+    return res.status(400).send({error:'user already exist'});
+
+  const hashPassword = await bcrypt.hash(password,10);
+
+  const newUser = await User.create({
+    fullName,
+    email,
+    password: hashPassword,
+    isAdmin});
 
   res.send({
     message:'User create',
