4b0b226f9725c6132a3f927d08782750df793131
prashanna <prashandon97@gmail.com>
2026-09-06 11:53:35 +0545

    add database seeder

--- backend/data/users.js
+++ backend/data/users.js
@@ -2,19 +2,19 @@ import bcrypt from "bcryptjs";
 
 const users = [
   {
-    name: "admin",
+    fullName: "admin",
     email: "admin@example.com",
     password: bcrypt.hashSync("password", 10),
     isAdmin: true,
   },
   {
-    name: "John Doe",
+    fullName: "John Doe",
     email: "john.doe@example.com",
     password: bcrypt.hashSync("password", 10),
     isAdmin: false,
   },
   {
-    name: "Jane Doe",
+    fullName: "Jane Doe",
     email: "jane.doe@example.com",
     password: bcrypt.hashSync("password", 10),
     isAdmin: false,
--- backend/seeder.js
+++ backend/seeder.js
@@ -1,24 +1,47 @@
-import User from './models/user.js';
-import Product from './models/products.js';
-import Order from './models/order.js';
-import mongoose from 'mongoose';
+import User from "./models/user.js";
+import Product from "./models/products.js";
+import Order from "./models/order.js";
+import mongoose from "mongoose";
+
+import products from "./data/products.js";
+import users from "./data/users.js";
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then((conn) => console.log(`Connecting to DB at ${conn.connection.host}`))
   .catch((err) => console.log("Error connecting to DB", err.message));
 
-  const destroyData = async ()=>{
-    try{
-      await User.deleteMany();
-      await Product.deleteMany();
-      await Order.deleteMany();
-      console.log("Data Destroyed");
-      process.exit(0);
-    }catch(err){
-      console.log("Error destroying data", err.message);
-      process.exit(1); //1 means error, 0 means success
-    };
-  };
+const importData = async () => {
+  try {
+    await User.deleteMany();
+    await Product.deleteMany();
+    await Order.deleteMany();
 
-  destroyData();
+    const addedUsers = await User.insertMany(users);
+    const adminId = addedUsers[0]._id;
+    const addedProducts = await Product.insertMany(
+      products.map((p) => ({ ...p, user: adminId })),
+    );
+
+    console.log("Data Imported");
+    process.exit(0);
+  } catch (err) {
+    console.log("Error destroying data", err.message);
+    process.exit(1); //1 means error, 0 means success
+  }
+};
+
+const destroyData = async () => {
+  try {
+    await User.deleteMany();
+    await Product.deleteMany();
+    await Order.deleteMany();
+    console.log("Data Destroyed");
+    process.exit(0);
+  } catch (err) {
+    console.log("Error destroying data", err.message);
+    process.exit(1); //1 means error, 0 means success
+  }
+};
+
+importData();
