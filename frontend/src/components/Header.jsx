30b5d7146fcba0230c27a97524f25d5e02327ed3
prashanna <prashandon97@gmail.com>
2026-09-11 08:12:48 +0545

    add product

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,11 @@
-import {Navbar,Nav,Container,NavDropdown} from "react-bootstrap"; 
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import logo from "../assets/react.svg";
-import { FaShoppingCart, FaSignInAlt} from "react-icons/fa";
-import {  } from "react-icons/fa";
+import { FaShoppingCart, FaSignInAlt } from "react-icons/fa";
+import {} from "react-icons/fa";
 
-
-function Header(){
-  return(
-    <Navbar bg="dark" variant="dark" expand="md" >
+function Header() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="md">
       <Container>
         <img src={logo} alt="logo" />
         <Navbar.Brand href="#home">HimalayaShop</Navbar.Brand>
@@ -15,19 +14,17 @@ function Header(){
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link href="#home">
-              <FaShoppingCart />
-
-              addTo Cart
+              
+              <FaShoppingCart />addTo Cart
             </Nav.Link>
             <Nav.Link href="#link">
-              <FaSignInAlt />
-              Signup
+              <FaSignInAlt />Signup
             </Nav.Link>
           </Nav>
           <NavDropdown id="basic-navbar-nav"></NavDropdown>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
+  );
 }
 export default Header;
---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,17 @@
+import { Card } from "react-bootstrap";
+
+function Product({ product }) {
+  return (
+    <Card className="my-3 p-3 rounds">
+      <Card.Img src={product.image} variant="top" />
+      <Card.Body>
+        <Card.Title>
+          <strong>{product.name}</strong>
+        </Card.Title>
+        <Card.Text as="h4">${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Product;
